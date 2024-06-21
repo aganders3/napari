d62811e5e3441a38c1f4b5bd7f8f57a067b8abb1
Ashley Anderson <aganders3@gmail.com>
2024-06-21 14:27:31 -0400

    Narrower tests, re-enable commands in toggle_play

--- napari/_qt/_qapp_model/_tests/test_view_menu.py
+++ napari/_qt/_qapp_model/_tests/test_view_menu.py
@@ -130,11 +130,11 @@ def test_toggle_play(make_napari_viewer, qtbot):
     data = np.random.random((10, 10, 15))
     viewer.add_image(data)
     # Assert action triggers play
-    # app.commands.execute_command(action_id)
-    # qtbot.waitUntil(_is_playing)
+    app.commands.execute_command(action_id)
+    qtbot.waitUntil(_is_playing)
     # Assert action stops play
-    # app.commands.execute_command(action_id)
-    # qtbot.waitUntil(_is_not_playing)
+    app.commands.execute_command(action_id)
+    qtbot.waitUntil(_is_not_playing)
 
 
 @skip_local_popups

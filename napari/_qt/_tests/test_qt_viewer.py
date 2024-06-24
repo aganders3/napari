926243303bca3d668ef66a65fa424b69cb3a0396
Ashley Anderson <aganders3@gmail.com>
2024-06-24 10:51:50 -0400

    enable_console in test

--- napari/_qt/_tests/test_qt_viewer.py
+++ napari/_qt/_tests/test_qt_viewer.py
@@ -81,10 +81,11 @@ def test_qt_viewer_toggle_console(make_napari_viewer):
 
 
 @skip_local_popups
+@pytest.mark.enable_console()
 @pytest.mark.skipif(os.environ.get('MIN_REQ', '0') == '1', reason='min req')
 def test_qt_viewer_console_focus(qtbot, make_napari_viewer):
     """Test console has focus when instantiating from viewer."""
-    viewer = make_napari_viewer(show=True, enable_console=True)
+    viewer = make_napari_viewer(show=True)
     view = viewer.window._qt_viewer
     assert not view.console.hasFocus(), 'console has focus before being shown'
 
